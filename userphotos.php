e8b8351f415253dba91721fee24f7f4481ce790e
Meelis Lutsar <31948961+Kuuskinen@users.noreply.github.com>
2017-12-07 10:48:18 +0200

    Alustatud tsitaatidega

--- userphotos.php
+++ userphotos.php
@@ -30,14 +30,30 @@
 <head>
 	<meta charset="utf-8">
 	<title>NASA kosmosefotod</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-<body>
+<body class="bg-secondary">
+<div class="container bg-secondary">
+<nav class="navbar navbar_inverse">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#"</a>
+		</div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="main.php">Kodu</a></li>
+      <li><a href="foto.php">Pildid</a></li>
+	  <li><a href="usersInfo.php">Kasutajate andmed</a></li>
+	  <li><a href="?logout=1">Logi välja</a></li>
+    </ul>
+  </div>
+</nav>
 	<h1>NASA kosmosefotod</h1>
-	<p>Ma püüan sellest midagi aretada ;)</p>
-	<p><a href="?logout=1">Logi välja!</a></p>
-	<p><a href="main.php">Pealeht</a></p>
+	<p>Siin näed oma üleslaetud tsitaate, saad neid muuta ja näed kõiki üleslaetud pilte.</p>
 	<hr>
-	<h2>Sinu laetud fotod ja tsitaadid</h2>
+	<h2>Tsitaadid</h2>
 	<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
 		<label>Lisa uus tsitaat</label>
 		<input name="userIdea" type="text">
@@ -46,7 +62,7 @@
 		<input name="ideaBtn" type="submit" value="Salvesta"><span><?php echo $notice; ?></span>
 	</form>
 	<hr>
-	<h2>Sisestatud tsitaadid</h2>
+	<h2>Sinu sisestatud</h2>
 	<div style="width: 40%">
 		<?php echo listIdeas(); ?>
 	</div>
